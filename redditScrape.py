f83a14d0f6427d2de4c701afd8a623fecb76a62b
carolinehardin <caroline.hardin@doit.wisc.edu>
2015-02-25 20:26:41 -0600

    changed to using praw for input

--- redditScrape.py
+++ redditScrape.py
@@ -3,21 +3,31 @@ from bs4 import BeautifulSoup
 import sys
 from urlparse import urlparse
 from tldextract import tldextract
-
 import logging
+import praw
+
 
 LOG_FILENAME = 'redditScraper.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 
-# We only want the 'description' field or we get any resource mentioned in the post title, so delete the 'title' column 
-#before making the input csv
+#create a file for saving the reddit stuff to
+f1 = open('prawScrape.html', 'r+')
+
+#let's grab the stuff from reddit using praw
+reddit = praw.Reddit(user_agent='example')
+lp = reddit.get_subreddit('learnprogramming')
+for x in lp.get_new(limit=100):
+    for c in x.comments:
+        f1.write(c.body_html.encode('utf-8'))
+
 
 f = open(sys.argv[1]) #remember sys.argv[0] is the name of the script
 					  #open defaults to read if no argument given
 
 resources = {} #create a dictionary to keep the resource names in and count the number of appearences
-soup = BeautifulSoup(f) #parse the document
+
+soup = BeautifulSoup(f1) #parse the document. we're using the praw version now
 links = soup.find_all('a') #find all a href link tags
 # we need to find markdown links also
 
