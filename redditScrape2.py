9b69428ad08a38176331a653f0521f9ef88f1fdc
Grant Dobbe <grant@dobbe.us>
2015-03-22 19:04:47 -0500

    - fixed all the file inputs/outputs - added more descriptive text - removed deadwood calls

--- redditScrape2.py
+++ redditScrape2.py
@@ -1,7 +1,10 @@
-import os, sys, logging, praw, HTMLParser, ConfigParser
+import os, logging, praw, HTMLParser, ConfigParser, pprint
 from bs4 import BeautifulSoup
 from urlparse import urlparse
 from tldextract import tldextract
+
+print "Reddit Research Scraper v0.1"
+print "============================"
 
 '''
 Grab the config file (we're gonna need it later on)
@@ -13,7 +16,9 @@ try:
   print "Settings parsed correctly."
 except ConfigParser.NoSectionError:
   print "Your config file does not appear to be valid. Please verify that settings.conf exists."
-  
+
+# gimme a pretty printer in case I need it later
+pp = pprint.PrettyPrinter(indent=4)  
 
 # log events to redditScraper.log with debug level logging
 LOG_FILENAME = config.get('global', 'logfile')
@@ -24,7 +29,7 @@ logging.basicConfig(filename=LOG_FILENAME, level=LOG_LEVEL)
 redditParse = HTMLParser.HTMLParser() 
 
 # create a file for saving the reddit stuff to
-scrapeOutput = open('prawScrape.html', 'r+')
+outputFile = config.get('global', 'outputFile')
 
 #let's grab the stuff from reddit using praw
 reddit = praw.Reddit(user_agent='linux:ResearchRedditScraper:v0.1 (by /u/plzHowDoIProgram)')
@@ -35,52 +40,58 @@ password = config.get('user', 'password')
 print "Logging into Reddit..."
 reddit.login(username, password)
 
-print "Requesting comments from reddit..."
+print "We're in. Requesting comments from reddit..."
 commentPile = reddit.get_comments('learnprogramming', limit=None)
-print type(commentPile)
-#print "We got " + str(size(commentPile)) + " comments from reddit."
 
-print "Request complete. Parsing raw comments..."
+print "Request complete. Parsing raw comments to HTML..."
 commentCount = 0
-for comments in commentPile:
-	scrapeOutput.write((redditParse.unescape(comments.body_html)).encode('utf-8')) #unescape the html and put in unicode
-	commentCount += 1
+with open(outputFile, 'w+') as scrapeOutput:
+	for comments in commentPile:
+		scrapeOutput.write((redditParse.unescape(comments.body_html)).encode('utf-8')) #unescape the html and put in unicode
+		commentCount += 1
 
 print "Complete. We parsed " + str(commentCount) + " comments."
 	
-#or get input through a file on the command line, this is much faster
-f = open(sys.argv[1]) #remember sys.argv[0] is the name of the script
-					  #open defaults to read if no argument given
+# parse the document. we're using the praw version now
+with open(outputFile, 'r') as scrapeOutput:
+	soup = BeautifulSoup(scrapeOutput) 
+	# find all a href link tags
+	linkPile = soup.find_all('a') 
+	# pretty print the output to see what we've got so far
+	#pp.pprint(linkPile)
+	print "We found " + str(len(linkPile)) + " total links in this pull."
 
-# create a dictionary to keep the resource names in and count the number of appearences
-resources = {} 
-#parse the document. we're using the praw version now
-htmlParse = BeautifulSoup(scrapeOutput) 
-# find all a href link tags
-linkPile = htmlParse.find_all('a') 
-# TODO: do we need to find markdown links also?
-
-for linkCandidate in linkPile:
-	print linkCandidate
-	baseUrl = urlparse(linkCandidate['href']).hostname
+	print "Building dictionary..."
+	# create a dictionary to keep the resource names in and count the number of appearences
+	resources = {} 
 	
-	if baseUrl:
-		resourceFound = tldextract.extract(baseUrl).domain # we only want top level domains
-		if resources.has_key(resourceFound): #check each name against this list to see if it's new
-			resources[resourceFound] = (resources[resourceFound]+1)  #if in list, increment count
-		else:
-			resources[resourceFound] = 1  #if not in list, add it 
-print resources
 
-#now go back through the file and find how many times in all text each resources shows up
+	# look at every link in the pile
+	for linkCandidate in linkPile:
+		# obtain the baseUrl
+		baseUrl = urlparse(linkCandidate['href']).hostname
+		
+		# if it's not empty
+		if baseUrl:
+			# extract the TLD
+			resourceFound = tldextract.extract(baseUrl).domain # we only want top level domains
+			if resources.has_key(resourceFound): #check each name against this list to see if it's new
+				resources[resourceFound] = (resources[resourceFound]+1)  #if in list, increment count
+			else:
+				resources[resourceFound] = 1  #if not in list, add it 
+	
+	print "Dictionary complete. \n Here's what I've found so far: "
+	pp.pprint(resources)
 
-with open(sys.argv[1],'r') as inputFile:
-	allText = inputFile.read()
+
+
+# now go back through the file and find how many times in all text each resources shows up
+with open(outputFile, 'r') as scrapeOutput:
+	allText = scrapeOutput.read()
 	for key in resources:
 		totalCount = 0
 		totalCount = allText.count(key)
-		print key, totalCount 
-	 
+		print key + " was referenced " + str(totalCount) + " times."
 
 
 
